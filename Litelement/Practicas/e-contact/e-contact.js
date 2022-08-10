47fdbd44335e6bdf5461104f1e471a687d77ad35
idsrobertosanchez <roberto.sanchez.a@ids.com.mx>
2022-08-10 17:45:27 -0500

    Lite Element

---
+++ Litelement/Practicas/e-contact/e-contact.js
@@ -0,0 +1,6 @@
+import { EContact } from './src/EContact.js';
+import { EContactList } from './src/EContactList.js';
+
+window.customElements.define('e-contact', EContact);
+window.customElements.define('l-contact', EContactList);
+
